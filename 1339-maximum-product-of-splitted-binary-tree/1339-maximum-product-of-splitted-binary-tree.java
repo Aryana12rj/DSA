caa5d6ec629a4867c0400f4b2b227fdfb75540fd
Aryana12rj <149641931+Aryana12rj@users.noreply.github.com>
2026-01-07 20:11:33 +0530

    Time: 15 ms (8.17%), Space: 62.9 MB (12.84%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.java
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.java
@@ -13,6 +13,21 @@
  *     }
  * }
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     long MOD = 1000000007L;
     long ans = 0;
