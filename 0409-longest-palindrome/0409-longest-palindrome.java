ad7d500abc5f7cac62aaf92669f69744b6c14500
Aryana12rj <149641931+Aryana12rj@users.noreply.github.com>
2026-01-11 20:20:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,22 +1,15 @@
-class Solution {
-    public int longestPalindrome(String s) {
-        if (s == null || s.length() == 0) return 0;
-
-        HashSet<Character> hs = new HashSet<>();
+public int longestPalindrome(String s) {
+        if(s==null || s.length()==0) return 0;
+        HashSet<Character> hs = new HashSet<Character>();
         int count = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i); 
-            if (hs.contains(ch)) {
+        for(int i=0; i<s.length(); i++){
+            if(hs.contains(s.charAt(i))){
+                hs.remove(s.charAt(i));
                 count++;
-                hs.remove(ch); // remove to complete the pair
-            } else {
-                hs.add(ch);
+            }else{
+                hs.add(s.charAt(i));
             }
         }
-
-        // If there are leftover characters, we can place one in the middle
-        if (!hs.isEmpty()) return count * 2 + 1;
-        return count * 2;
-    }
+        if(!hs.isEmpty()) return count*2+1;
+        return count*2;
 }
