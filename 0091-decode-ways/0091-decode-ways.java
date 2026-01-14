aa790d51bfa714392e7ce3cd625bc6898483fd88
Aryana12rj <149641931+Aryana12rj@users.noreply.github.com>
2026-01-14 19:36:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,14 +1,16 @@
-class Solution{
-
-    	public int numDecodings(String s) {
-        int dp1=1, dp2=0, n=s.length();
-        for(int i=n-1;i>=0;i--) {
-            int dp=s.charAt(i)=='0'?0:dp1;
-            if(i<n-1&&(s.charAt(i)=='1'||s.charAt(i)=='2'&&s.charAt(i+1)<'7'))
-                dp+=dp2;
-            dp2=dp1;
-            dp1=dp;
-        }
-        return dp1;
+	public int numDecodings(String s) {
+        int n=s.length();
+        Integer[] mem=new Integer[n];
+        return s.length()==0?0:numDecodings(0,s,mem);      
     }
-}
+    private int numDecodings(int p, String s, Integer[] mem) {
+        int n=s.length();
+        if(p==n) return 1;
+        if(s.charAt(p)=='0') return 0;
+        if(mem[p]!=null) return mem[p];
+        int res=numDecodings(p+1,s,mem);
+        if(p<n-1&&(s.charAt(p)=='1'||s.charAt(p)=='2'&&s.charAt(p+1)<'7')) 
+			res+=numDecodings(p+2,s,mem);
+        return mem[p]=res;
+    }
+    
