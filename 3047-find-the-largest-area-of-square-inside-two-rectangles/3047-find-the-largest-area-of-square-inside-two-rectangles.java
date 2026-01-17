05ef3d4c0f76a490fa1395a19426cb5df8a94eaa
Aryana12rj <149641931+Aryana12rj@users.noreply.github.com>
2026-01-17 09:54:28 +0530

    Time: 79 ms (52.08%), Space: 47.7 MB (87.5%) - LeetHub

--- 3047-find-the-largest-area-of-square-inside-two-rectangles/3047-find-the-largest-area-of-square-inside-two-rectangles.java
+++ 3047-find-the-largest-area-of-square-inside-two-rectangles/3047-find-the-largest-area-of-square-inside-two-rectangles.java
@@ -1,22 +1,19 @@
 class Solution {
-    public long largestSquareArea(int[][] bl, int[][] tr) {
-        int s = 0;
-        int n = bl.length;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int minX = Math.max(bl[i][0], bl[j][0]);
-                int maxX = Math.min(tr[i][0], tr[j][0]);
-                int minY = Math.max(bl[i][1], bl[j][1]);
-                int maxY = Math.min(tr[i][1], tr[j][1]);
-
-                if (minX < maxX && minY < maxY) {
-                    int len = Math.min(maxX - minX, maxY - minY);
-                    s = Math.max(s, len);
-                }
+    public long largestSquareArea(int[][] bottomLeft, int[][] topRight) {
+        int n = bottomLeft.length; long maxArea = 0;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                int left = Math.max(bottomLeft[i][0], bottomLeft[j][0]);
+                int right = Math.min(topRight[i][0], topRight[j][0]);
+                int top = Math.min(topRight[i][1], topRight[j][1]);
+                int bottom = Math.max(bottomLeft[i][1], bottomLeft[j][1]);
+                if(left < right && top > bottom){
+                    int height = top - bottom; int width = right - left;
+                    int side = Math.min(height, width);
+                    maxArea = Math.max(maxArea, 1L * side * side);
+                }   
             }
         }
-
-        return (long) s * s;
+        return maxArea;
     }
 }
