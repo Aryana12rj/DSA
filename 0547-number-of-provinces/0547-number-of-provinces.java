d6a0d98227b68e9bef8c0fef7be88c162214e0ee
Aryana12rj <149641931+Aryana12rj@users.noreply.github.com>
2026-03-02 23:47:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,23 +1,50 @@
-class Solution {
-    private void dfs(int node,int[][] isConnected,boolean[] vis,int n){
-        vis[node]=true;
-        for(int i=0;i<n;i++){
-            if(isConnected[node][i]==1&&!vis[i]){
-                dfs(i,isConnected,vis,n);
-            }
-        }
-    }
-    public int findCircleNum(int[][] isConnected) {
-        int n=isConnected.length;
-        boolean[] vis=new boolean[n];
-        int provinces=0;//for count of provinces
+class DSU{
+    int[] parent,size;
+    DSU(int n){
+        parent=new int[n];
+        size=new int[n];
 
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,isConnected,vis,n);
-                provinces++;
+            parent[i]=i;
+            size[i]=1;
+
+        }
+    }
+    public  int findparent(int x){
+        if(parent[x]==x)return x;
+        return parent[x]=findparent(parent[x]);
+    }
+    public void unionbysize(int u,int v){
+        int pu=findparent(u);
+        int pv=findparent(v);
+        if(pu==pv)return ;
+        if(size[u]<size[v]){
+            parent[pu]=pv;
+            size[pv]+=size[pu];
+        }else{
+            parent[pv]=pu;
+            size[pu]+=size[pv];
+        }
+    }
+
+}
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int n=isConnected.length;
+        DSU dsu=new DSU(n);
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(isConnected[i][j]==1){
+                    dsu.unionbysize(i,j);
+                }
+
             }
         }
+        int provinces=0;
+        for(int i=0;i<n;i++){
+            if(dsu.findparent(i)==i)provinces++;
+        }
+
         return provinces;
     }
 }
