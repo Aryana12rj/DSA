b8c58ac9373dbbd2e8dac2bda1a617f0835e8290
Aryana12rj <149641931+Aryana12rj@users.noreply.github.com>
2026-03-03 00:10:16 +0530

    Time: 52 ms (32.15%), Space: 46.3 MB (55.02%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,27 +1,20 @@
 class Solution {
-    private int tabuFunc(int[] nums, int[][] dp) {
-        int n = nums.length;
-        for (int index = n - 1; index >= 0; index--) {
-            for (int prevIndex = index - 1; prevIndex >= -1; prevIndex--) {
+    public int lengthOfLIS(int[] nums) {
+        int n=nums.length;
+        int[] dp=new int[n];
 
-                // Not take case
-                int notTake = dp[index + 1][prevIndex + 1];
-
-                // Take case
-                int take = 0;
-                if (prevIndex == -1 || nums[index] > nums[prevIndex]) {
-                    take = 1 + dp[index + 1][index + 1];
+        for(int i=0;i<n;i++){
+            dp[i]=1;
+        }
+        int maxlen=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
                 }
-
-                dp[index][prevIndex + 1] = Math.max(take, notTake);
+                maxlen=Math.max(maxlen,dp[i]);
             }
         }
-        return dp[0][0];
-    }
-
-    public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int[][] dp = new int[n + 1][n + 1];
-        return tabuFunc(nums, dp);
+        return maxlen;
     }
 }
