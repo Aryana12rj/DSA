178fe534fb39f6465f7aeb6fa3bf4b54d40c51eb
Aryana12rj <149641931+Aryana12rj@users.noreply.github.com>
2026-03-13 12:43:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,28 +1,27 @@
 public class Solution {
     public String longestPalindrome(String s) {
-        if (s.length() <= 1) {
+        if(s.length()<=1){
             return s;
         }
+        int maxlen=1;
+        int start=0;
+        int end=0;
+        boolean[][] dp=new boolean[s.length()][s.length()];
+        for(int i=0;i<s.length();++i){
+            dp[i][i]=true;
+        
+        for(int j=0;j<i;++j){
+            if(s.charAt(j)==s.charAt(i)&& (i-j<=2 ||dp[j+1][i-1])){
+                dp[j][i]=true;
 
-        int maxLen = 1;
-        int start = 0;
-        int end = 0;
-        boolean[][] dp = new boolean[s.length()][s.length()];
-
-        for (int i = 0; i < s.length(); ++i) {
-            dp[i][i] = true;
-            for (int j = 0; j < i; ++j) {
-                if (s.charAt(j) == s.charAt(i) && (i - j <= 2 || dp[j + 1][i - 1])) {
-                    dp[j][i] = true;
-                    if (i - j + 1 > maxLen) {
-                        maxLen = i - j + 1;
-                        start = j;
-                        end = i;
-                    }
+                if(i-j+1>maxlen){
+                    maxlen=i-j+1;
+                    start=j;
+                    end=i;
+                }
                 }
             }
         }
-
-        return s.substring(start, end + 1);
+        return s.substring(start,end+1);
     }
 }
