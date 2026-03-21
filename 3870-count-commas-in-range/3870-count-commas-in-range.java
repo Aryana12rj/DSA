df8289b246938568feb874b83f6acd5c2e268b97
Aryana12rj <149641931+Aryana12rj@users.noreply.github.com>
2026-03-21 10:43:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,12 +1,9 @@
 class Solution {
     public int countCommas(int n) {
-        int ans=0;
-        for(int i=1;i<=n;i++){
-            //digit count
-            int digit=(int)(Math.log10(i))+1;
-            //comma based on digit length
-            ans+=(digit-1)/3;
+        if(n<1000){
+            return 0;
+        }else{
+            return n-1000+1;
         }
-        return ans;
     }
 }
