2cd963a37c8311c14045327342004f88f319a5d7
Aryana12rj <149641931+Aryana12rj@users.noreply.github.com>
2026-04-17 12:27:02 +0530

    Time: 12 ms (27.99%), Space: 46.5 MB (26.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        Map<Character,Integer> map=new HahMap<>();
+        Map<Character,Integer> map=new HashMap<>();
         //count freq of char in string s
-        for(char x:s.charArray()){
-            map.put(x,mp.getOrDefault(x,0)+1);
+        for(char x:s.toCharArray()){
+            map.put(x,map.getOrDefault(x,0)+1);
 
         }
         //decrement the freq of character in string t
-        for(char y:t.charArray()){
-            mp.put(y,mp.getOrDefault(y,0)-1);
+        for(char y:t.toCharArray()){
+            map.put(y,map.getOrDefault(y,0)-1);
         }
-        for(int count:mp.values()){
+        for(int count:map.values()){
             if(count!=0){
                 return false;
             }
