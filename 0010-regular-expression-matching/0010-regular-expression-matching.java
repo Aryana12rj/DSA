3844bac330d93743304014eba49348a18269e1a4
Aryana12rj <149641931+Aryana12rj@users.noreply.github.com>
2026-04-17 21:44:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,25 +1,27 @@
 class Solution {
-    public boolean solve(String s,String p){
-        if(p.length()==0){
-            if(s.length()==0){
-                return true;
-            }
-            return false;
-        }
-        boolean first_catch_match=false;
-        if(s.length()> 0 &&(p.charAt(0)==s.charAt(0)||p.charAt(0)=='.')){
-            first_catch_match=true;
-        }
-        if(p.length()>=2 &&p.charAt(1)=='*'){
-            boolean nottake=solve(s,p.substring(2));
-            boolean take=first_catch_match && solve(s.substring(1),p);
-            return nottake || take;
-        }else{
-            return first_catch_match && solve(s.substring(1),p.substring(1));
+    public boolean isMatch(String s, String p) {
+        return solve(s, p, 0, 0);
+    }
+
+    private boolean solve(String s, String p, int i, int j) {
+        // If pattern is finished, string must also be finished
+        if (j == p.length()) {
+            return i == s.length();
         }
 
-    }
-    public boolean isMatch(String s, String p) {
-        return solve(s,p);
+        // First character match check
+        boolean firstMatch = (i < s.length() &&
+                             (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.'));
+
+        // Handle '*' (look ahead one character in pattern)
+        if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+            // Case 1: skip "x*" entirely
+            // Case 2: use "x*" if firstMatch
+            return solve(s, p, i, j + 2) ||
+                   (firstMatch && solve(s, p, i + 1, j));
+        } else {
+            // Normal case: move both forward if match
+            return firstMatch && solve(s, p, i + 1, j + 1);
+        }
     }
 }
