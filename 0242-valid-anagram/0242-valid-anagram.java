718744540ba4f4f41b76b8f7522113b16634803c
Aryana12rj <149641931+Aryana12rj@users.noreply.github.com>
2026-04-17 11:06:14 +0530

    Time: 5 ms (61.73%), Space: 44.2 MB (91%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,23 +1,20 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        //if length is not same then not valid anagram
         if(s.length()!=t.length()){
             return false;
         }
-        //frequency character 
-        int[] charcnt=new int[26];
-        //for every character increment count for s and decrement length of t
-        
+        int[] count=new int[26];
         for(int i=0;i<s.length();i++){
-            charcnt[s.charAt(i)-'a']++;
-            charcnt[t.charAt(i)-'a']--;
+            count[s.charAt(i)-'a']++;
+            count[t.charAt(i)-'a']--;
 
         }
-        for(int cnt:charcnt){
-            if(cnt!=0){
-                return false;
-            }
+        for(int chec: count){
+           if(chec!=0){
+            return false;
+           } 
         }
         return true;
+        
     }
 }
