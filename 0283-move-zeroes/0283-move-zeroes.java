5375aaef1f59044a3c9592601613beb4492c200d
Aryana12rj <149641931+Aryana12rj@users.noreply.github.com>
2026-04-18 09:09:25 +0530

    Time: 2 ms (91.21%), Space: 47.9 MB (42.34%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i=0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=0){
+                swap(nums,i,j);
+                i++;
+            }
+        }
+    }
+    public void swap(int[] nums,int i,int j){
+        nums[i]=(nums[i]+nums[j])-(nums[j]=nums[i]);
+    }
+}
