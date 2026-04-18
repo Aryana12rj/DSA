1f117ad2e7cceba787086d7b76a4e2ad9dea3bea
Aryana12rj <149641931+Aryana12rj@users.noreply.github.com>
2026-04-18 13:21:44 +0530

    Time: 409 ms (5%), Space: 47.9 MB (65.7%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,12 +1,29 @@
 class Solution {
-    public boolean canJump(int[] nums) {
-        int reachable=0;
-        for(int i=0;i<nums.length;i++){
-            if(i>reachable){
-                return false;
-            }
-            reachable=Math.max(reachable,i+nums[i]);
+    public boolean solve(int[] nums,int n,int index,int[] dp){
+        if(index==n-1){
+            return true;
         }
-        return true;
+        if(index>=n){
+            return false;
+        }
+        if(dp[index]!=-1){
+            return dp[index]==1;
+        }
+        for(int i=1;i<=nums[index];i++){
+            if(solve(nums,n,index+i,dp)==true){
+                 dp[index]=1;
+                 return true;
+
+            }
+        }
+        dp[index]=0;
+        return false;
+
+    }
+    public boolean canJump(int[] nums) {
+        int n=nums.length;
+        int[] dp=new int[n];
+        Arrays.fill(dp,-1);
+        return solve(nums,n,0,dp);
     }
 }
