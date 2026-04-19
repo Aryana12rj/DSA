2a534d8baab1cd9d5d9f18abbc97672e054de331
Aryana12rj <149641931+Aryana12rj@users.noreply.github.com>
2026-04-20 00:55:54 +0530

    Time: 0 ms (100%), Space: 46.7 MB (42.06%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -1,17 +1,31 @@
-public class Solution {
-    int max;
-    
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maxvalue;
     public int maxPathSum(TreeNode root) {
-        max= Integer.MIN_VALUE;
-        maxPathDown(root);
-        return max;
+        maxvalue=Integer.MIN_VALUE;
+        maxpathdown(root);
+        return maxvalue;
+        
     }
-    
-    private int maxPathDown(TreeNode node) {
-        if (node == null) return 0;
-        int left = Math.max(0, maxPathDown(node.left));
-        int right = Math.max(0, maxPathDown(node.right));
-        max= Math.max(max, left + right + node.val);
-        return Math.max(left, right) + node.val;
+    private int maxpathdown(TreeNode root){
+        if(root==null)return 0;
+        int left=Math.max(0,maxpathdown(root.left));
+        int right=Math.max(0,maxpathdown(root.right));
+        maxvalue=Math.max(maxvalue,left+right+root.val);
+        return Math.max(left,right)+root.val;
     }
 }
