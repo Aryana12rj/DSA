be204eb986768dd5b25e55d7a04901018a34edfd
Aryana12rj <149641931+Aryana12rj@users.noreply.github.com>
2026-04-23 18:16:35 +0530

    Time: 20 ms (78.16%), Space: 54 MB (53.08%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -7,11 +7,11 @@ class Solution {
             for(int j=1;j<=m;j++){
                 if(text1.charAt(i-1)==text2.charAt(j-1)){
                     dp[i][j]=1+dp[i-1][j-1];
+
                 }else{
                     dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
         return dp[n][m];
-    }
-}
+}}
