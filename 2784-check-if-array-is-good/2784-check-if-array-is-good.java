7fa6fbbbcec9c8bdf7ee0b291c4bde010ac14d3f
Aryana12rj <149641931+Aryana12rj@users.noreply.github.com>
2026-05-14 23:31:39 +0530

    Time: 1 ms (91.05%), Space: 44.6 MB (70.86%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.java
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.java
@@ -1,34 +1,21 @@
 class Solution {
     public boolean isGood(int[] nums) {
-        int maxx = 0;
+        int n = nums.length - 1;
+        Set<Integer> seen = new HashSet<>();
+        boolean dup = false;
 
-        for (int x : nums) {
-            maxx = Math.max(maxx, x);
+        for (int num : nums) {
+            if (num > n) return false;
+
+            if (seen.contains(num)) {
+                if (num < n || dup) return false;
+                dup = true;
+                continue;
+            }
+
+            seen.add(num);
         }
 
-
-        // size must be mx + 1
-        if (nums.length != maxx + 1)
-            return false;
-
-        int[] freq = new int[maxx + 1];
-
-        for (int x : nums) {
-            // invalid number
-            if (x < 1 || x > maxx)
-                return false;
-
-            freq[x]++;
-        }
-
-        // 1 to mx-1 should appear once
-        for (int i = 1; i < maxx; i++) {
-
-            if (freq[i] != 1)
-                return false;
-        }
-
-        // mx should appear twice
-        return freq[maxx] == 2;
+        return true;
     }
 }
