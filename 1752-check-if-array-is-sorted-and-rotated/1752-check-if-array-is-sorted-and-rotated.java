2dd59ef50e381161453dd1beb57da0fa8093ebb7
Aryana12rj <149641931+Aryana12rj@users.noreply.github.com>
2026-05-23 14:22:32 +0530

    Time: 0 ms (100%), Space: 42.7 MB (94.96%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,21 +1,22 @@
 class Solution {
     public boolean check(int[] nums) {
-        int n = nums.length;
-        for (int k = 0; k < n; k++) {
-            if (isSortedAfterRotation(nums, k)) {
+        int n=nums.length;
+        for(int k=0;k<n;k++){
+            if(issortedafterRotation(nums,k)){
                 return true;
             }
         }
         return false;
-    }
-    private boolean isSortedAfterRotation(int[] nums, int k) {
-        int n = nums.length;
 
-        for (int i = 0; i < n - 1; i++) {
-            int curr = nums[(i + k) % n];
-            int next = nums[(i + 1 + k) % n];
-            if (curr > next) {
-                return false; // break in order
+
+    }
+    public boolean issortedafterRotation(int[] nums,int k){
+        int n=nums.length;
+        for(int i=0;i<n-1;i++){
+            int curr=nums[(i+k)%n];
+            int next=nums[(i+1+k)%n];
+            if(curr>next){
+                return false;
             }
         }
         return true;
