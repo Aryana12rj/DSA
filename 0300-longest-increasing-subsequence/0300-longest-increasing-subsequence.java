008e4ede061492e223900a40b9b14cd434d26756
Aryana12rj <149641931+Aryana12rj@users.noreply.github.com>
2026-06-30 18:22:19 +0530

    Time: 49 ms (32.41%), Space: 46.2 MB (41.62%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,41 +1,21 @@
-import java.util.*;
-
 class Solution {
     int n;
-    int[] nums;
-
-    public int solve(int indx, int prev, int[][] dp) {
-        if (indx == n) return 0;
-
-        // Only check memo if prev is valid
-        if (prev != -1 && dp[indx][prev] != -1) {
-            return dp[indx][prev];
-        }
-
-        // Option 1: skip current
-        int skip = solve(indx + 1, prev, dp);
-
-        // Option 2: take current if valid
-        int take = 0;
-        if (prev == -1 || nums[prev] < nums[indx]) {
-            take = 1 + solve(indx + 1, indx, dp);
-        }
-
-        // Store result only if prev is valid
-        if (prev != -1) {
-            dp[indx][prev] = Math.max(take, skip);
-        }
-
-        return Math.max(take, skip);
-    }
-
     public int lengthOfLIS(int[] nums) {
-        this.n = nums.length;
-        this.nums = nums;
+        n=nums.length;
+        int maxlenlis=1;
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,1);
 
-        int[][] dp = new int[n+1][n + 1];
-        for (int[] row : dp) Arrays.fill(row, -1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                }
+            }
+            maxlenlis=Math.max(maxlenlis,dp[i]);
+        }
+        return maxlenlis;
 
-        return solve(0, -1, dp);
+
     }
 }
