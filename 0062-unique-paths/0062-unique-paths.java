fa02efb51ee27c69e7afd5aaf84e8dd2139ea668
Aryana12rj <149641931+Aryana12rj@users.noreply.github.com>
2026-07-02 21:31:44 +0530

    Time: 0 ms (100%), Space: 42.1 MB (65.33%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,24 +1,22 @@
 class Solution {
-    public int solve(int i,int j,int m,int n,int[][] dp){
-        if(i==m-1 && j==n-1){
-            return 1;
-
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(i<0||i>=m||j<0||j>=n){
-            return 0;
-        }
-        int down=solve(i+1,j,m,n,dp);
-        int right=solve(i,j+1,m,n,dp);
-        return dp[i][j]=down+right;
-    }
     public int uniquePaths(int m, int n) {
         int[][] dp=new int[m+1][n+1];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        dp[0][0]=1;//total ways to reach from (0,0) to (0,0) is 0 but her its is said to be one path that it 
+        //fill oth row
+        for(int col=1;col<n;col++){
+            dp[0][col]=1;
         }
-        return solve(0,0,m,n,dp);
+        //fill 0th col
+        for(int row=1;row<m;row++){
+            dp[row][0]=1;
+        }
+        //for remainiing 2d array we fill the dp table
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        //answer
+        return dp[m-1][n-1];
     }
 }
