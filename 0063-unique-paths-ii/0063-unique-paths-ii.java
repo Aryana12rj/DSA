ab50d4bed7ac3df2b70174e5611faeed214c1cc8
Aryana12rj <149641931+Aryana12rj@users.noreply.github.com>
2026-07-03 00:28:12 +0530

    Time: 0 ms (100%), Space: 43.3 MB (80.73%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,43 +1,44 @@
 import java.util.*;
 
 class Solution {
-    public int solve(int i, int j, int m, int n, int[][] dp, int[][] obstacleGrid) {
-        // edge cases and managing obstacle FIRST
-        if (i >= m || j >= n || i < 0 || j < 0 || obstacleGrid[i][j] == 1) {
-            return 0;
-        }
-
-        // if we reach destination
-        if (i == m - 1 && j == n - 1) {
-            return 1;
-        }
-
-        // precomputation
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        // move right and down
-        int right = solve(i, j + 1, m, n, dp, obstacleGrid);
-        int down = solve(i + 1, j, m, n, dp, obstacleGrid);
-
-        return dp[i][j] = right + down;
-    }
-
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
 
-        // if start or end is blocked
-        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) {
-            return 0;
+        int[][] dp = new int[m][n];
+
+        // fill first cell
+        dp[0][0] = (obstacleGrid[0][0] == 1) ? 0 : 1;
+
+        // fill first row
+        for (int col = 1; col < n; col++) {
+            if (obstacleGrid[0][col] == 1) {
+                dp[0][col] = 0;
+            } else {
+                dp[0][col] = dp[0][col - 1];
+            }
         }
 
-        int[][] dp = new int[m][n];   // ✅ fixed size
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
+        // fill first col
+        for (int row = 1; row < m; row++) {
+            if (obstacleGrid[row][0] == 1) {
+                dp[row][0] = 0;
+            } else {
+                dp[row][0] = dp[row - 1][0];
+            }
         }
 
-        return solve(0, 0, m, n, dp, obstacleGrid);
+        // fill rest of grid
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
     }
 }
