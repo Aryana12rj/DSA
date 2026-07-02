4d9d5e58d4ea7b034f3accf0558f20446e9fbf6a
Aryana12rj <149641931+Aryana12rj@users.noreply.github.com>
2026-07-03 00:36:30 +0530

    Time: 4 ms (63.97%), Space: 50.4 MB (37.67%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,36 +1,22 @@
 class Solution {
-    public int f(int i, int j, int[][] dp, int[][] grid) {
-        // Base case
-        if (i == 0 && j == 0) {
-            return grid[0][0];
-        }
-        // Out of bounds
-        if (i < 0 || j < 0) {
-            return (int)1e9;
-        }
-        // Memoization check
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        
-        // Recursive calls
-        int up = grid[i][j] + f(i - 1, j, dp, grid);
-        int left = grid[i][j] + f(i, j - 1, dp, grid);
-        
-        // Store and return minimum
-        return dp[i][j] = Math.min(up, left);
-    }
-    
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         
-        // Initialize dp with -1
-        int[][] dp = new int[m][n];
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
+        for (int i = 1; i < m; i++) {
+            grid[i][0] += grid[i-1][0];
         }
         
-        return f(m - 1, n - 1, dp, grid);
+        for (int j = 1; j < n; j++) {
+            grid[0][j] += grid[0][j-1];
+        }
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                grid[i][j] += Math.min(grid[i-1][j], grid[i][j-1]);
+            }
+        }
+        
+        return grid[m-1][n-1];
     }
 }
